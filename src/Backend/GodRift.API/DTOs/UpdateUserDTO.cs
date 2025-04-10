e012ecf270ea9c976bdf1e67c4ae70f52ad58740
belluccaz <lucas.bellucci@icloud.com>
2025-04-10 05:22:23 -0300

    chaging structure to match DDD

--- src/Backend/GodRift.API/DTOs/UpdateUserDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace GodRift.API.DTOs
-{
-    public class UpdateUserDTO
-    {
-        public long UserId { get; set; }
-        public required string Name { get; set; }
-        public required string Email { get; set; }
-    }
-}
